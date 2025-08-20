72219d667d4f2f47d6fe8d056450b71680072266
Rishi <rishitj1972@gmail.com>
2025-08-20 20:31:43 +0530

    project-setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+const App = () => {
+  return (
+    <div className="text-blue-500">
+      <Button>
+        Click me
+      </Button>
+    </div>
+  )
+}
+
+export default App
