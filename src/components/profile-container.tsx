a4a8fd1f88eb92ab92624046d1c544e44e272368
Rishi <rishitj1972@gmail.com>
2025-08-21 14:09:40 +0530

    added the header component ,added the mobile responsive nav section and added the profile icon at header section

---
+++ src/components/profile-container.tsx
@@ -0,0 +1,23 @@
+import { useAuth, UserButton } from "@clerk/clerk-react";
+import { Loader } from "lucide-react";
+import { Button } from "./ui/button";
+import { Link } from "react-router";
+
+const ProfileContainer = () => {
+
+    const {isSignedIn, isLoaded} = useAuth(); {/* isSIgnedIn and isLoaded is specified in the layout protected routes*/}
+
+    if(!isLoaded) {
+  return (
+    <div className="flex items-center">
+        <Loader className="min-w-4 min-h-4 animate-spin text-emerald-500"/>
+    </div>
+  );
+}
+
+return <div className="flex items-center gap-6">
+    {isSignedIn ? ( <UserButton />) : (<Link to={"/signin"}><Button size={"sm"}>Get Started</Button></Link>)}
+</div>
+};
+
+export default ProfileContainer
---
+++ src/lib/helpers.ts
@@ -0,0 +1,18 @@
+export const MainRoutes = [
+    {
+        label : "Home",
+        path: "/",
+    },
+    {
+        label : "Contact Us",
+        path: "/contact",
+    },
+    {
+        label: "About Us",
+        path: "/about",
+    },
+    {
+        label: "Services",
+        path: "/services",
+    },
+]; 
