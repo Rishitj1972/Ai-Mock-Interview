dd4fb6a2703f3522bcd2cb6e0dc0ff8ec95df24d
Rishi <rishitj1972@gmail.com>
2025-09-02 09:25:19 +0530

    gemini ai is integrated and prompts to create questions and answers then store that data's into the firebase then fetched from there and show it in the dashboard as position title only

--- src/components/form-mock-interview.tsx
+++ src/components/form-mock-interview.tsx
@@ -1,7 +1,6 @@
-import {z} from "zod";
+import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import {FormProvider, useForm} from "react-hook-form";
-
+import { FormProvider, useForm } from "react-hook-form";
 
 import type { Interview } from "@/types";
 import CustomBreadCrumb from "./custom-bread-crumb";
@@ -13,216 +12,313 @@ import Headings from "./headings";
 import { Loader, Trash2 } from "lucide-react";
 import { Button } from "./ui/button";
 import { Separator } from "./ui/separator";
-import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
+import { chatSession } from "@/scripts";
+import { addDoc, collection, doc, serverTimestamp, updateDoc } from "firebase/firestore";
+import { db } from "@/config/firebase.config";
 
 interface FormMockInterviewProps {
-    initialData: Interview | null; // initial data for the form from the parent component which is the create-edit-page which supplies the interview data
+  initialData: Interview | null; // initial data for the form from the parent component which is the create-edit-page which supplies the interview data
 }
 
 // Define the form schema using Zod
 const formSchema = z.object({
-    
-    position: z.string()
+  position: z
+    .string()
     .min(1, "Position is required")
     .max(100, "Position must be at most 100 characters or less"),
-    
-    description: z.string().min(10, "Description is required"),
-    
-    experience: z.coerce.number().min(0, "Experience must be at least 0 years"),
-    
-    techStack: z.string().min(1, "Tech Stack is required")
+
+  description: z.string().min(10, "Description is required"),
+
+  experience: z.coerce.number().min(0, "Experience must be at least 0 years"),
+
+  techStack: z.string().min(1, "Tech Stack is required"),
 });
 
 type FormData = z.infer<typeof formSchema>; // Infer means to extract the form data type from the schema
 
 const FormMockInterview = ({ initialData }: FormMockInterviewProps) => {
-  
-    const form = useForm<FormData>({
-        resolver: zodResolver(formSchema) as any, 
-        defaultValues: initialData || {
-            position: "",
-            description: "",
-            experience: 0,
-            techStack: ""
-        },
-    });
+  const form = useForm<FormData>({
+    resolver: zodResolver(formSchema) as any,
+    defaultValues: initialData || {
+      position: "",
+      description: "",
+      experience: 0,
+      techStack: "",
+    },
+  });
 
-    const { isValid, isSubmitting } = form.formState; // Destructure form state
-    const [loading, setLoading] = useState(false); // Loading state for form submission
-    const navigate = useNavigate(); // Navigation hook
-    const { userId } = useAuth();  // User ID from authentication
+  const { isValid, isSubmitting } = form.formState; // Destructure form state
+  const [loading, setLoading] = useState(false); // Loading state for form submission
+  const navigate = useNavigate(); // Navigation hook
+  const { userId } = useAuth(); // User ID from authentication
 
-    const title = initialData?.position ? initialData?.position : "Create a new Mock Interview"; // Form title based on create or edit mode
+  const title = initialData?.position
+    ? initialData?.position
+    : "Create a new Mock Interview"; // Form title based on create or edit mode
 
-    const breadCrumbPage = initialData?.position ? "Edit" : "Create"; // Breadcrumb page title based on create or edit mode
+  const breadCrumbPage = initialData?.position ? "Edit" : "Create"; // Breadcrumb page title based on create or edit mode
 
-    const actions = initialData?.position ? "Save changes" : "Create"; // Button text based on create or edit mode
-    
-    const toastMessage = initialData 
-    ? {title: "Updated..!",description: "Changes saved successfully.."}
-    : {title: "Created..!",description: "New mock interview created..!"};
+  const actions = initialData?.position ? "Save changes" : "Create"; // Button text based on create or edit mode
 
+  const toastMessage = initialData
+    ? { title: "Updated..!", description: "Changes saved successfully.." }
+    : { title: "Created..!", description: "New mock interview created..!" };
 
-    const onSubmit = async (data: FormData) => {
-        try {
-            console.log(data);
-            setLoading(true);
-        } catch (error) {
-            console.log(error);
-            toast.error("Error..", {
-                description: "Something went wrong..!"
-            })
-        } finally {
-            setLoading(false);
-        }
+  // clean AI Response
+
+  const cleanAiResponse = (responseText: string) => {
+    // trim any surrounding whitespaces
+    let cleanText = responseText.trim();
+    const jsonArrayMatch = cleanText.match(/\[.*\]/s); // Match JSON array using regex
+    if (jsonArrayMatch) {
+      cleanText = jsonArrayMatch[0];
+    } else {
+      throw new Error("No JSON array found in the response");
     }
 
-    useEffect(() => {
-        if(initialData) {
-            form.reset({
-                position: initialData.position,
-                description: initialData.description,
-                experience: Number(initialData.experience) || 0,
-                techStack: initialData.techStack
-            })
-        }
-    },[initialData, form])
-  
-  return (<div className="w-full flex-col space-y-4">
-    <CustomBreadCrumb
-        breadCrumbPage={breadCrumbPage} // Breadcrumb page title
-        breadCrumbItems={[{link:"/generate", label:"Mock Interview"}]}
-    />
-    <div className="mt-4 flex items-center justify-between w-full">
-    <Headings
-        title={title} isSubheading
-    />
-    {initialData && (
-        <Button
-        size = {"icon"}
-        variant={"ghost"}
-        >
-            <Trash2 className="min-w-4 min-h-4 text-red-500"/>
-        </Button>
-    )}
-    </div>
-    <Separator className="my-4"/>
-    <div className="my-6"></div>
+    // parse the clean json text into an array of objects
 
-    <FormProvider {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="w-full p-8 rounded-lg flex flex-col gap-6 shadow-md"> 
-            
-            {/* Position Field */}
-            <FormField
+    try {
+      return JSON.parse(cleanText);
+    } catch (error) {
+      throw new Error(
+        `Failed to parse JSON array: ${(error as Error).message}`
+      );
+    }
+  };
+
+  // creating AI response and prompt generation
+
+  const generateAiResponse = async (data: FormData) => {
+    const prompt = `As an experienced prompt engineer, generate a JSON array containing 5
+        technical interview question and 2 coding questions(1 easy-level and 1 medium-level without 
+        specifying the programming language) along with detailed answer based on the following job information.
+        Each object in the array should have the fields "question" and "answer" formatted as follows:
+        
+        [
+            {"question": "<Question text>","answer": "<Answer text>"},
+            ...
+        ]
+        
+        Job Information:
+        - Job Position: ${data.position}
+        - Job Description: ${data.description}
+        - Year of Experience Required: ${data.experience}
+        - Tech Stack: ${data.techStack}
+
+        The questions should asses skills in ${data?.techStack} development and best practices,
+        problem solving, and experienced handling complex requirements.Please format the output 
+        strictly as an array of JSON objects without any additional labels, or explanations.
+        Return only the JSON array with questions and answers.
+        `;
+
+    const aiResult = await chatSession.sendMessage(prompt);
+    const cleanedAiResponse = cleanAiResponse(aiResult.response.text().trim()); // Get the return response as cleanText
+    
+    return cleanedAiResponse; 
+  };
+
+  // Form submission handler
+  const onSubmit = async (data: FormData) => {
+    try {
+      setLoading(true);
+      if (initialData) {
+        // update Process
+
+        if(isValid){
+            const aiResult = await generateAiResponse(data);
+
+            await updateDoc(doc(db,"interviews",initialData?.id),{
+                questions : aiResult,
+                ...data,
+                updatedAt : serverTimestamp()
+            });
+            toast(toastMessage.title, {description : toastMessage.description});
+        }
+
+      } else {
+        // Create new mock interview
+        if (isValid) {
+          const aiResult = await generateAiResponse(data);
+
+        await addDoc(collection(db,"interviews"),{
+            ...data,
+            userId,
+            questions : aiResult,
+            createdAt : serverTimestamp(),
+          });
+
+          toast(toastMessage.title, {description : toastMessage.description});
+
+        }
+      }
+      navigate("/generate",{replace: true});
+
+    } catch (error) {
+      console.log(error);
+      toast.error("Error..", {
+        description: "Something went wrong..!",
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (initialData) {
+      form.reset({
+        position: initialData.position,
+        description: initialData.description,
+        experience: Number(initialData.experience) || 0,
+        techStack: initialData.techStack,
+      });
+    }
+  }, [initialData, form]);
+
+  return (
+    <div className="w-full flex-col space-y-4">
+      <CustomBreadCrumb
+        breadCrumbPage={breadCrumbPage} // Breadcrumb page title
+        breadCrumbItems={[{ link: "/generate", label: "Mock Interview" }]}
+      />
+      <div className="mt-4 flex items-center justify-between w-full">
+        <Headings title={title} isSubheading />
+        {initialData && (
+          <Button size={"icon"} variant={"ghost"}>
+            <Trash2 className="min-w-4 min-h-4 text-red-500" />
+          </Button>
+        )}
+      </div>
+      <Separator className="my-4" />
+      <div className="my-6"></div>
+
+      <FormProvider {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full p-8 rounded-lg flex flex-col gap-6 shadow-md"
+        >
+          {/* Position Field */}
+          <FormField
             control={form.control}
             name="position"
-            render={({field})=>(
-                <FormItem className="w-full">
-                    <div className="w-full flex items-center justify-between">
-                        <FormLabel>Job Role</FormLabel>
-                        <FormMessage className="text-sm"/>
-                    </div>
-                    <FormControl>
-                        <Input
-                            {...field}
-                            disabled={loading}
-                            placeholder="eg:- full stack developer"
-                            className="h-12"
-                        />
-                    </FormControl>
-                </FormItem>
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <div className="w-full flex items-center justify-between">
+                  <FormLabel>Job Role</FormLabel>
+                  <FormMessage className="text-sm" />
+                </div>
+                <FormControl>
+                  <Input
+                    {...field}
+                    disabled={loading}
+                    placeholder="eg:- full stack developer"
+                    className="h-12"
+                  />
+                </FormControl>
+              </FormItem>
             )}
-            />
+          />
 
-            {/* description */}
-            <FormField
+          {/* description */}
+          <FormField
             control={form.control}
             name="description"
-            render={({field})=>(
-                <FormItem className="w-full space-y-4">
-                    <div className="w-full flex items-center justify-between">
-                        <FormLabel>Job Description</FormLabel>
-                        <FormMessage className="text-sm"/>
-                    </div>
-                    <FormControl>
-                        <Textarea
-                            disabled={loading}
-                            placeholder="eg:- describe the job role"
-                            className="h-12"
-                            {...field}
-                        />
-                    </FormControl>
-                </FormItem>
+            render={({ field }) => (
+              <FormItem className="w-full space-y-4">
+                <div className="w-full flex items-center justify-between">
+                  <FormLabel>Job Description</FormLabel>
+                  <FormMessage className="text-sm" />
+                </div>
+                <FormControl>
+                  <Textarea
+                    disabled={loading}
+                    placeholder="eg:- describe the job role"
+                    className="h-12"
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
             )}
-            />
+          />
 
-            {/* experience */}
-            <FormField
+          {/* experience */}
+          <FormField
             control={form.control}
             name="experience"
-            render={({field})=>(
-                <FormItem className="w-full space-y-4">
-                    <div className="w-full flex items-center justify-between">
-                        <FormLabel>Years of Experience</FormLabel>
-                        <FormMessage className="text-sm"/>
-                    </div>
-                    <FormControl>
-                        <Input
-                            type="number"
-                            disabled={loading}
-                            placeholder="eg:- 5 years in number"
-                            className="h-12"
-                            {...field}
-                        />
-                    </FormControl>
-                </FormItem>
+            render={({ field }) => (
+              <FormItem className="w-full space-y-4">
+                <div className="w-full flex items-center justify-between">
+                  <FormLabel>Years of Experience</FormLabel>
+                  <FormMessage className="text-sm" />
+                </div>
+                <FormControl>
+                  <Input
+                    type="number"
+                    disabled={loading}
+                    placeholder="eg:- 5 years in number"
+                    className="h-12"
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
             )}
-            />
+          />
 
-            {/* tech stacks */}
-            <FormField
+          {/* tech stacks */}
+          <FormField
             control={form.control}
             name="techStack"
-            render={({field})=>(
-                <FormItem className="w-full space-y-4">
-                    <div className="w-full flex items-center justify-between">
-                        <FormLabel>Tech Stacks</FormLabel>
-                        <FormMessage className="text-sm"/>
-                    </div>
-                    <FormControl>
-                        <Textarea
-                            disabled={loading}
-                            placeholder="eg:- React,Node.js etc."
-                            className="h-12"
-                            {...field}
-                        />
-                    </FormControl>
-                </FormItem>
+            render={({ field }) => (
+              <FormItem className="w-full space-y-4">
+                <div className="w-full flex items-center justify-between">
+                  <FormLabel>Tech Stacks</FormLabel>
+                  <FormMessage className="text-sm" />
+                </div>
+                <FormControl>
+                  <Textarea
+                    disabled={loading}
+                    placeholder="eg:- React,Node.js etc."
+                    className="h-12"
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
             )}
-            />
-            <div className="w-full flex items-center justify-end gap-6">
-                <Button
-                    type="reset"
-                    size={"sm"}
-                    variant={"outline"}
-                    disabled={isSubmitting || loading}
-                >
-                    Reset
-                </Button>
-                <Button
-                    type="submit"
-                    size={"sm"}
-                    disabled={isSubmitting || loading || !isValid}
-                >
-                    {loading ? ( <Loader className="text-gray-50 animate-spin" /> ) : ( actions )}
-                </Button>
-            </div>
+          />
+          <div className="w-full flex items-center justify-end gap-6">
+            <Button
+              type="reset"
+              size={"sm"}
+              variant={"outline"}
+              disabled={isSubmitting || loading}
+            >
+              Reset
+            </Button>
+            <Button
+              type="submit"
+              size={"sm"}
+              disabled={isSubmitting || loading || !isValid}
+            >
+              {loading ? (
+                <Loader className="text-gray-50 animate-spin" />
+              ) : (
+                actions
+              )}
+            </Button>
+          </div>
         </form>
-    </FormProvider>
-  </div>
+      </FormProvider>
+    </div>
   );
-}
+};
 
-export default FormMockInterview
+export default FormMockInterview;
--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -1,9 +1,53 @@
 import Headings from "@/components/headings"
 import { Button } from "@/components/ui/button"
+import { Skeleton } from "@/components/ui/skeleton"
+import { db } from "@/config/firebase.config"
+import type { Interview } from "@/types"
+import { useAuth } from "@clerk/clerk-react"
+import { Separator } from "@radix-ui/react-separator"
+import { collection, onSnapshot, query, where } from "firebase/firestore"
 import { Plus } from "lucide-react"
+import { useEffect, useState } from "react"
 import { Link } from "react-router"
+import { toast } from "sonner"
+import { set } from "zod"
 
 const Dashboard = () => {
+
+    const [interviews,setInterviews] = useState<Interview[]>([])  // State to hold interviews
+    const [loading, setLoading] = useState(false); 
+    const {userId} = useAuth();
+
+    useEffect(() => {
+        setLoading(true);
+        const interviewQuery = query(
+            collection(db,"interviews"),
+            where("userId","==",userId)
+        );
+
+        const unsubscribe = onSnapshot(interviewQuery,(snapshot) => {
+            const interviewList : Interview[] = snapshot.docs.map((doc) => {
+                const id = doc.id
+                return {
+                    id,
+                    ...doc.data() 
+                }
+            }) as Interview[];
+
+            setInterviews(interviewList);
+            setLoading(false);
+        },
+            (error) => {
+                console.error("Error fetching interviews: ", error);
+                toast.error("Error...",{
+                    description : "There was an error fetching your interviews. Please try again later."
+                });
+                setLoading(false);
+            })
+
+            return () => unsubscribe();
+    },[userId])
+
   return <>
         <div className="flex w-full items-center justify-between">
             {/* headings */}
@@ -17,8 +61,14 @@ const Dashboard = () => {
                     <Plus/>Add New
                 </Button>
             </Link>
-            {/* content section */}
         </div>
+        <Separator className="my-8"/>
+        {/* content section */}
+            <div className="md:grid md:grid-cols-3 gap-3 py-4">
+                {loading ? Array.from({length : 6}).map((_,index) => (
+                    <Skeleton key={index} className="h-24 md:h-32 rounded-md"/>
+                )): interviews.length > 0 ? interviews.map((interview) => <p key={interview.id}>{interview.position}</p> ) :<div>No Data Found</div>}
+            </div>
     </>
 }
 
